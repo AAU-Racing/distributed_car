007818d0ff913c6e2862025666e830e2b6706a61
PB16 <peterbugtrup97@gmail.com>
2019-11-05 17:15:06 +0100

    changes

--- board_driver/uart.c
+++ board_driver/uart.c
@@ -1,102 +1,219 @@
-#include <stm32f4xx_hal.h>
 #include <stdio.h>
+#include <stm32f4xx.h>
 
+#include "gpio.h"
 #include "uart.h"
 #include "ringbuffer.h"
 
 #define BUF_SIZE 1024
-
-UART_HandleTypeDef UartHandle;
-ringbuffer_t uartx_rec;
-ringbuffer_t uartx_send;
-uint8_t uartx_rec_buf[BUF_SIZE];
-uint8_t uartx_send_buf[BUF_SIZE];
+#define CHANNELS 3
+#define CONFIGS 8
+#define INSTANCES 4
 
 
-void HAL_UART_MspInit(UART_HandleTypeDef *huart) {
-	(void)huart;
+#define UART_INSTANCE ((USART_TypeDef* const []) { \
+    USART1,	\
+    USART6, \
+    USART3, \
+    USART2, \
+    USART6, \
+    USART6, \
+    USART2, \
+    USART1  \
+})
 
-	USARTx_TX_GPIO_CLK_ENABLE();
-	USARTx_RX_GPIO_CLK_ENABLE();
-	USARTx_CLK_ENABLE();
+#define UART_RX_PIN ((const GPIO_Pin const []) { \
+    PIN_7,  \
+    PIN_7,  \
+    PIN_11, \
+    PIN_6,  \
+    PIN_7,  \
+    PIN_7,  \
+    PIN_6,  \
+    PIN_7   \
+})
 
-	GPIO_InitTypeDef gpio_init = {
-		.Pin       = USARTx_TX_PIN,
-		.Mode      = GPIO_MODE_AF_PP,
-		.Pull      = GPIO_PULLUP,
-		.Speed     = GPIO_SPEED_FAST,
-		.Alternate = USARTx_TX_AF,
-	};
-	HAL_GPIO_Init(USARTx_TX_GPIO_PORT, &gpio_init);
-	gpio_init.Pin       = USARTx_RX_PIN;
-	gpio_init.Alternate = USARTx_RX_AF;
-	HAL_GPIO_Init(USARTx_RX_GPIO_PORT, &gpio_init);
+#define UART_RX_PORT ((GPIO_TypeDef* const []) { \
+    GPIOB, \
+    GPIOC, \
+    GPIOB, \
+    GPIOD, \
+    GPIOC, \
+    GPIOC, \
+    GPIOD, \
+    GPIOB \
+})
 
-	// Insert rx / tx DMA here
+#define UART_TX_PIN ((const GPIO_Pin const []) { \
+    PIN_6,  \
+    PIN_6,  \
+    PIN_10,	\
+    PIN_5,  \
+    PIN_6,  \
+    PIN_6,  \
+    PIN_5,  \
+    PIN_6  \
+})
 
-	HAL_NVIC_SetPriority(USARTx_IRQn, 0, 0);
-	HAL_NVIC_EnableIRQ(USARTx_IRQn);
+#define UART_TX_PORT ((GPIO_TypeDef* const []) { \
+    GPIOB, \
+    GPIOC, \
+    GPIOB, \
+    GPIOD, \
+    GPIOC, \
+    GPIOC, \
+    GPIOD, \
+    GPIOB  \
+})
+
+#define UART_AF ((const GPIO_Pin const []) { \
+    GPIO_AF7, \
+    GPIO_AF8, \
+    GPIO_AF7, \
+    GPIO_AF7, \
+    GPIO_AF8, \
+    GPIO_AF8, \
+    GPIO_AF7, \
+    GPIO_AF7  \
+})
+
+typedef struct {
+    ringbuffer_t rb_tx;
+    ringbuffer_t rb_rx;
+    uint8_t buffer_tx[BUF_SIZE];
+    uint8_t buffer_rx[BUF_SIZE];
+} uart_channel;
+
+uart_channel channels[CHANNELS];
+
+uint8_t channel_configs[CONFIGS] = { 255, 255, 255, 255, 255, 255, 255, 255 };
+uint8_t current_channel = 0;
+uint8_t instance_channels[INSTANCES] = {255, 255, 255, 255};
+
+static void init_rb(uint8_t config) {
+    uart_channel* channel = channels + channel_configs[config];
+
+    rb_init(&channel->rb_rx, channel->buffer_rx, BUF_SIZE);
+    rb_init(&channel->rb_tx, channel->buffer_tx, BUF_SIZE);
 }
 
-void HAL_UART_MspDeInit(UART_HandleTypeDef *huart) {
-	(void)huart;
-
-	USARTx_FORCE_RESET();
-	USARTx_RELEASE_RESET();
-
-	HAL_GPIO_DeInit(USARTx_TX_GPIO_PORT, USARTx_TX_PIN);
-	HAL_GPIO_DeInit(USARTx_RX_GPIO_PORT, USARTx_RX_PIN);
+static void init_gpio(uint8_t config) {
+    gpio_af_init(UART_TX_PORT[config], UART_TX_PIN[config], GPIO_HIGH_SPEED, GPIO_PUSHPULL, UART_AF[config]);
+    gpio_af_init(UART_RX_PORT[config], UART_RX_PIN[config], GPIO_HIGH_SPEED, GPIO_PUSHPULL, UART_AF[config]);
 }
 
-void uart_init(void) {
-	UartHandle.Instance = USARTx;
 
-	UartHandle.Init.BaudRate     = 115200;
-	UartHandle.Init.WordLength   = UART_WORDLENGTH_8B;
-	UartHandle.Init.StopBits     = UART_STOPBITS_1;
-	UartHandle.Init.Parity       = UART_PARITY_NONE;
-	UartHandle.Init.HwFlowCtl    = UART_HWCONTROL_NONE;
-	UartHandle.Init.Mode         = UART_MODE_TX_RX;
-	UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
+void uart_init(uint8_t config) {
+    channel_configs[config] = current_channel++;
+    init_rb(config);
+    init_gpio(config);
 
-	rb_init(&uartx_rec, uartx_rec_buf, BUF_SIZE);
-	rb_init(&uartx_send, uartx_send_buf, BUF_SIZE);
-	HAL_UART_Init(&UartHandle);
-	SET_BIT(UartHandle.Instance->CR1, USART_CR1_RXNEIE);
+    USART_TypeDef* instance = UART_INSTANCE[config];
+
+    if (instance == USART1){
+        instance_channels[0] = channel_configs[config];
+        SET_BIT(RCC->APB2ENR, RCC_APB2ENR_USART1EN);
+
+        NVIC_SetPriority(USART1_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0x1, 0x0));
+        NVIC_EnableIRQ(USART1_IRQn);
+    } else if (instance == USART2) {
+        instance_channels[1] = channel_configs[config];
+        SET_BIT(RCC->APB1ENR, RCC_APB1ENR_USART2EN);
+
+        NVIC_SetPriority(USART2_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0x1, 0x1));
+        NVIC_EnableIRQ(USART2_IRQn);
+    } else if (instance == USART3) {
+        instance_channels[2] = channel_configs[config];
+        SET_BIT(RCC->APB1ENR, RCC_APB1ENR_USART3EN);
+
+        NVIC_SetPriority(USART3_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0x1, 0x2));
+        NVIC_EnableIRQ(USART3_IRQn);
+
+    } else if (instance == USART6) {
+        instance_channels[3] = channel_configs[config];
+        SET_BIT(RCC->APB2ENR, RCC_APB2ENR_USART6EN);
+
+        NVIC_SetPriority(USART6_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0x1, 0x3));
+        NVIC_EnableIRQ(USART6_IRQn);
+    }
+
+    SET_BIT(instance->CR1, USART_CR1_TE);
+    SET_BIT(instance->CR1, USART_CR1_RE);
+
+    if (instance == USART1 || instance == USART6) {
+        // APB2 = 80 MHz
+        // 80 MHz / (115200 baud * 16) = 43.402777778
+        // 0.402777778 * 16 = 6.444444448, floor(6.444444448) = 0x6
+        // 43 << 4 = 0x2b0
+        // 0x2b0 + 0x6 = 0x2b6
+        instance->BRR = 0x2b6;
+    }
+    else {
+        // APB1 = 40 MHz
+        // 40 MHz / (115200 baud * 16) = 21.701388889
+        // 0.701388889 * 16 = 11.222222224, floor(11.222222224) = 0xb
+        // 21 << 4 = 0x150
+        // 0x150 + 0xb = 0x15b
+        instance->BRR = 0x015b;
+    }
+
+    SET_BIT(instance->CR1, USART_CR1_RXNEIE);
+    SET_BIT(instance->CR1, USART_CR1_UE);
 }
 
-void USARTx_IRQHandler(void) {
-	uint32_t isrflags = READ_REG(UartHandle.Instance->SR);
+void USARTx_IRQHandler(uint8_t config, USART_TypeDef* instance) {
+    uart_channel* channel = channels + instance_channels[config];
 
-	if (isrflags & USART_SR_RXNE) {
-		uint8_t data_in = (uint8_t)(UartHandle.Instance->DR & (uint8_t)0x00FF);
-		rb_push(&uartx_rec, data_in);
-	}
+    if (READ_BIT(instance->SR, USART_SR_RXNE)) {
+        uint8_t data_in = (uint8_t) (instance->DR & 0xFF);
+        rb_push(&channel->rb_rx, data_in);
+    }
 
-	if (isrflags & USART_SR_TXE) {
-		uint8_t data_out;
-		if (rb_pop(&uartx_send, &data_out)) {
-			CLEAR_BIT(UartHandle.Instance->CR1, USART_CR1_TXEIE);
-		} else {
-			UartHandle.Instance->DR = data_out & (uint8_t)0x00FF;
-		}
-	}
+    if (READ_BIT(instance->SR, USART_SR_TXE)) {
+        uint8_t data_out;
+        if (rb_pop(&channel->rb_tx, &data_out) == 1) {
+            CLEAR_BIT(instance->CR1, USART_CR1_TXEIE); // no data available
+        } else {
+            instance->DR = data_out;
+        }
+    }
 }
 
-uint8_t uart_read_byte(void) {
-	uint8_t data;
-	while (rb_pop(&uartx_rec, &data));
-	return data;
+void USART1_IRQHandler() {
+    USARTx_IRQHandler(0, USART1);
 }
 
-void uart_send_byte(uint8_t data) {
-	while (rb_isFull(&uartx_send));
-	rb_push(&uartx_send, data);
-	SET_BIT(UartHandle.Instance->CR1, USART_CR1_TXEIE);
+void USART2_IRQHandler() {
+    USARTx_IRQHandler(1, USART2);
 }
 
-void uart_send_buf(uint8_t *data, size_t n) {
-	for (size_t i = 0; i < n; i++) {
-		uart_send_byte(data[i]);
-	}
+void USART3_IRQHandler() {
+    USARTx_IRQHandler(2, USART3);
+}
+
+void USART6_IRQHandler() {
+    USARTx_IRQHandler(3, USART6);
+}
+
+uint8_t uart_read_byte(uint8_t config) {
+    uint8_t data;
+    uart_channel* channel = channels + channel_configs[config];
+    while (rb_pop(&channel->rb_rx, &data));
+    return data;
+}
+
+void uart_send_byte(uint8_t config, uint8_t data) {
+    uart_channel* channel = channels + channel_configs[config];
+    USART_TypeDef* instance = UART_INSTANCE[config];
+
+    while (rb_isFull(&channel->rb_tx));
+
+    rb_push(&channel->rb_tx, data);
+    SET_BIT(instance->CR1, USART_CR1_TXEIE);
+}
+
+void uart_send_buf(uint8_t config, uint8_t *data, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        uart_send_byte(config, data[i]);
+    }
 }
